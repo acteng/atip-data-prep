c52d2979f52674b750bae832ec281cf7032408f2
Dustin Carlino <dabreegster@gmail.com>
2022-10-31 17:27:24 +0000

    Overhasty

--- split_uk_osm.sh
+++ split_uk_osm.sh
@@ -9,7 +9,7 @@ fi
 mkdir uk_osm
 cd uk_osm
 mkdir out
-../geojson_to_osmium_extracts.py ../authorities.geojson --name_key=Name --output_dir=out/ --batch_size=2
+../geojson_to_osmium_extracts.py ../authorities.geojson --name_key=name --output_dir=out/ --batch_size=5
 
 for batch in osmium_cfg_*; do
 	time osmium extract -v -c $batch ../england-latest.osm.pbf
