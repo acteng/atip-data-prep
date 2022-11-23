85a64e55ea683fb94b5bdd864f7276cdee29e027
Dustin Carlino <dabreegster@gmail.com>
2022-11-23 16:27:16 +0000

    Use pueue

--- build_route_snappers.sh
+++ build_route_snappers.sh
@@ -11,12 +11,12 @@ mkdir -p route-snappers
 IFS=$'\n'
 for x in uk_osm/out/*; do
 	geojson=$(basename $x .osm).geojson
-	cargo run --release $x uk_osm/$geojson
-	mv *.bin route-snappers
+	pueue add --escape cargo run --release "$x" "uk_osm/$geojson"
 done
 
 # Put in S3
-aws s3 sync --dry route-snappers s3://abstreet/route-snappers/
+#mv *.bin route-snappers
+#aws s3 sync --dry route-snappers s3://abstreet/route-snappers/
 
 # Have to invalidate the CDN
 # Go to https://us-east-1.console.aws.amazon.com/cloudfront/v3/home?region=us-east-2#/distributions/ER5B3SJO4ND9D/invalidations/create
