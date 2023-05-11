59c4997325c2cb6b23da0e0b799fd133f4a8842b
Dustin Carlino <dabreegster@gmail.com>
2023-05-11 18:54:19 +0100

    Switch route_info to using osm2streets directly, using a simple CLI tool to import

--- build_route_info.sh
+++ build_route_info.sh
@@ -5,23 +5,26 @@
 set -e
 
 # Build the importer
-cd abstreet
-./import.sh
-# Clean anything from a previous import
-rm -rf data/system/zz/oneshot/maps/
+cd importer
+cargo build --release
 cd ..
+
+# Clean anything from a previous import
+rm -rf route_info_files
+mkdir route_info_files
 
 IFS=$'\n'
 for osm in uk_osm/out/*; do
 	geojson=$(basename $osm .osm).geojson
-	pueue add -w abstreet --escape ./target/release/cli oneshot-import "../$osm" --clip-path "../uk_osm/$geojson" --skip-ch
+	out=$(basename $osm .osm).bin
+	pueue add -w importer --escape cargo run --release -- "../$osm" "../uk_osm/$geojson" "../route_info_files/$out"
 done
 
 # Manually wait for pueue to finish
 
 # Put in S3
-# prod: aws s3 sync --dry abstreet/data/system/zz/oneshot/maps/ s3://atip.uk/route-info/
-# dev: aws s3 sync --dry abstreet/data/system/zz/oneshot/maps/ s3://atip.uk/route-info-dev/
+# prod: aws s3 sync --dry route_info_files/ s3://atip.uk/route-info/
+# dev: aws s3 sync --dry route_info_files/ s3://atip.uk/route-info-dev/
 # If the same files are going in dev and prod, this saves bandwidth: aws s3 sync --dry s3://atip.uk/route-info/ s3://atip.uk/route-info-dev/
 
 # Have to invalidate the CDN manually! Use the S3 console
