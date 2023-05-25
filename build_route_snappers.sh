24f3aebd00e079e7d5174ea6919ca838b6593d9a
Dustin Carlino <dabreegster@gmail.com>
2023-05-25 17:18:26 +0100

    Gzip files after generating them

--- build_route_snappers.sh
+++ build_route_snappers.sh
@@ -16,8 +16,11 @@ IFS=$'\n'
 for osm in uk_osm/out/*; do
 	geojson=$(basename $osm .osm).geojson
 	out=$(basename $osm .osm).bin
-	pueue add --escape $bin -i "$osm" -b "uk_osm/$geojson" -o "route-snappers/$out"
+	task=$(pueue add --print-task-id --escape $bin -i "$osm" -b "uk_osm/$geojson" -o "route-snappers/$out")
+	pueue add --after $task --escape gzip "route-snappers/$out"
 done
+
+# Manually wait for pueue to finish
 
 # Put in S3
 # prod: aws s3 sync --dry route-snappers s3://atip.uk/route-snappers/
