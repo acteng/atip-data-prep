5d5b95f53121cbda4af6fed3d592aa62a35f0902
Peter York <peter.york@dft.gov.uk>
2023-11-15 15:05:39 +0000

    Script to update the browse all schemes geojson format

---
+++ all_schemes_geojson_prep/index.ts
@@ -0,0 +1,24 @@
+const { readFileSync, writeFileSync } = require("fs");
+
+const allSchemes =  JSON.parse(readFileSync("./data/input/all_scheme_data.geojson"));
+
+const resultingSchemeDictionary = {};
+Object.keys(allSchemes.schemes).forEach((browseSchemeKey) => {
+    const scheme = allSchemes.schemes[browseSchemeKey];
+    const result = {
+        scheme_name: browseSchemeKey,
+        browse: scheme
+    }
+    resultingSchemeDictionary[browseSchemeKey] = result;
+});
+
+allSchemes.schemes = resultingSchemeDictionary;
+const outputPath = "./data/output/all_scheme_data.geojson"; 
+
+try {
+    writeFileSync(outputPath, JSON.stringify(allSchemes));
+    console.log(`Data successfully output to ${outputPath}`)
+
+} catch (error) {
+    console.log(`Error writing new all schemes geojson: ${error}`)
+}
