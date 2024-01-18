f840ca1fb69653eff9863650c6f23f9135f02761
Dustin Carlino <dabreegster@gmail.com>
2024-01-18 16:17:38 +0000

    Start generating a basic stats19 dataset. https://github.com/acteng/atip/issues/280

---
+++ stats19/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p input
+# URLs from https://www.data.gov.uk/dataset/cb7ae6f0-4be6-4935-9277-47e5ce24a11f/road-safety-data
+if [ ! -e "input/dft-road-casualty-statistics-casualty-1979-latest-published-year.csv" ]; then
+	wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-casualty-1979-latest-published-year.csv -P input
+fi
+if [ ! -e "input/dft-road-casualty-statistics-collision-1979-latest-published-year.csv" ]; then
+	wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-collision-1979-latest-published-year.csv -P input
+fi
+
+mkdir -p tmp
+time cargo run --release
+
+# TODO This is still generating tiles < 9 with a big size, but... no points?!
+time tippecanoe tmp/stats19.geojson --generate-ids -l stats19 -Z9 -zg --drop-densest-as-needed --extend-zooms-if-still-dropping -o stats19.pmtiles
