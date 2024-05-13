d6e6510097e3456d2fb788d6e039885a87ebba16
Dustin Carlino <dabreegster@gmail.com>
2024-05-13 11:07:05 +0100

    Don't round OS road width

--- ordnance_survey_layers/src/width.rs
+++ ordnance_survey_layers/src/width.rs
@@ -4,8 +4,8 @@ pub fn road_width_properties(
     input: &gdal::vector::Feature,
     output: &mut geojson::Feature,
 ) -> Result<bool> {
-    let average = input.field_as_integer_by_name("roadwidth_average")?;
-    let minimum = input.field_as_integer_by_name("roadwidth_minimum")?;
+    let average = input.field_as_double_by_name("roadwidth_average")?;
+    let minimum = input.field_as_double_by_name("roadwidth_minimum")?;
 
     if let (Some(average), Some(minimum)) = (average, minimum) {
         output.set_property("average", average);
